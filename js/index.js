7a9d470fb55872f702a3133a0014ef8978cdeacf
sheiladai <326278902@qq.com>
2015-08-11 11:53:10 +0800

    社区文章接口

--- js/index.js
+++ js/index.js
@@ -5,9 +5,100 @@
 /*globals jQuery, document */
 (function ($) {
     "use strict";
+    var topics = new Array(6);
+    //调用imweb社区最新文章接口
+    latestTopics = function(topic){
+        var link = "http://imweb.io/topic/" + topic._id;
+
+        var summary = "<p>" + topic.summary + "<p>";
+        summary = summary.replace(/<(?:.|\n)*?>/gm, '').substr(0, 140);
+
+        var excerptLink = "<p><a href='" + link + "' class='excerpt-link'>Read More...</a></p>";
+
+        var time = topic.create_at;
+        var timeShow = time.substr(0,10);
+        var timeSpan = "<time datetime='" + time +"' itemprop='datePublished'>"+ timeShow + "</time>";
+
+        var tab = topic.tab;
+        switch (tab) {
+            case "html":
+                tab = "HTML&HTML5";
+                break;
+            case "rebuild":
+                tab = "CSS/重构";
+                break;
+            case "network":
+                tab = "HTTP网络";
+                break;
+            case "secure":
+                tab = "Web安全";
+                break;
+            case "browser":
+                tab = "浏览器";
+                break;
+            case "debug":
+                tab = "调试";
+                break;
+            case "build":
+                tab = "构建工具";
+                break;
+            case "performance":
+                tab = "性能";
+                break;
+            case "lib":
+                tab = "前端库";
+                break;
+            case "node":
+                tab = "nodeJS全栈";
+                break;
+            case "mobile":
+                tab = "移动开发";
+                break;
+            case "tools":
+                tab = "工具建设";
+                break;
+            case "op":
+                tab = "运营";
+                break;
+            case "frame":
+                tab = "基础库";
+                break;
+            case "js":
+                tab = "javascript技术";
+                break;
+        }
+
+        var tabLink = "<a href='http://imweb.io/tab/"+ tab  + "'>" + tab + "</a>";
+
+        var postMeta = "<span class='post-meta'>" + timeSpan + " | " + tabLink + "</span>";
+        var postTitle = "<h3 class='post-title'><a href='" +link +"'>" + topic.title + "</a></h3>";
+        var section = "<section class='post-excerpt'>" + summary + excerptLink + "</section>";
+        var article = "<article class='post index-post'><header class='post-header'>" + postMeta + postTitle + "</header>" + section + "</article>";
+        $(".latest-article").append(article);
+    }
+
 
     $(document).ready(function(){
-
+    	$.ajax({
+			type:"get",
+			async:false,
+		    url:"http://imweb.io/topics/latestTopics",
+		    dataType:"jsonp",
+			jsonp: "callback",
+            jsonpCallback:"callback",
+            data:{
+            	topics: topics
+            },
+            success: function(callback){
+            	for(var i in callback){
+            		topics[i] = callback[i];
+                    latestTopics(topics[i]);
+            	}
+            },
+            error: function(){
+                alert('fail');
+             }
+		});
         $(".post-content").fitVids();
         $(".site-head").interactive_bg({
         	/*
