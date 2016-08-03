252308dc5c9adaed65c0c5d26ef0cca59c15278b
jerojiang(江源) <jerojiang@tencent.com>
2016-08-03 16:10:12 +0800

    [imui]fix font path

--- imui/external/index.js
+++ imui/external/index.js
@@ -5,70 +5,71 @@ define('external/index', function(require, exports, module) {
    * @author jero
    * @date 2016-08-02
    */
-
+  
   'use strict';
-
+  
   function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
+  
   var _jquery = require('external/jquery');
-
+  
   var _jquery2 = _interopRequireDefault(_jquery);
-
+  
   var $win = (0, _jquery2['default'])(window);
   var hostname = location.hostname;
   var pathname = location.pathname;
-
+  
   var M = {
     init: function init() {
       this.size();
       this.events();
       this.$iframe = (0, _jquery2['default'])('iframe:first');
       this.$a = (0, _jquery2['default'])('ul').find('a');
-
+  
       this.$iframe.prop('src', this.parseHref(this.$a.first().addClass('active').prop('href')));
     },
-
+  
     parseHref: function parseHref(href) {
-        var ret = href; // 默认是 dev 的时候
-
-        if (pathname.indexOf('imui') > -1) {
-          ret = href.replace('/components', '/imui/components');
-        }
-
-        return ret;
+      var ret = href; // 默认是 dev 的时候
+  
+      if (pathname.indexOf('imui') > -1) {
+        // check imui 目录
+        ret = href.replace('/components', '/imui/components');
+      }
+  
+      return ret;
     },
-
+  
     events: function events() {
       var _this = this;
-
+  
       $win.on('resize', function () {
         _this.size();
       });
-
+  
       (0, _jquery2['default'])(document).on('click', 'li a', function (e) {
         e.preventDefault();
-
+  
         var $target = (0, _jquery2['default'])(e.target);
-
+  
         if ($target.hasClass('active')) {
           return;
         }
-
+  
         _this.$a.removeClass('active');
         $target.addClass('active');
-
+  
         _this.$iframe.prop('src', _this.parseHref(e.target.href));
       });
     },
-
+  
     size: function size() {
       var headerHeight = (0, _jquery2['default'])('h1:first').outerHeight(true);
       var winHeight = $win.height();
-
+  
       (0, _jquery2['default'])('#main').height(winHeight - headerHeight);
     }
   };
-
+  
   M.init();
 
 });
